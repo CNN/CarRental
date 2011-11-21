7b977668d2acf1009e26de58c79e0ed94c9fc515
Niels Abildgaard <niels.abildgaard@gmail.com>
2011-11-21 17:28:06 +0100

    First commit

---
+++ src/carrental/CarRental.java
@@ -0,0 +1,14 @@
+package carrental;
+
+/**
+ *
+ * @author hypesystem
+ */
+public class CarRental {
+    
+    
+    public static void main(String[] args) {
+        System.out.println("run");
+    }
+    
+}
