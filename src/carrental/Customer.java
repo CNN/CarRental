ad5b2f22a105d2b3ac8bc3401ec8154354bea527
Claus L. Henriksen <clih@itu.dk>
2011-11-25 20:02:30 +0100

    Customer completed

--- src/carrental/Customer.java
+++ src/carrental/Customer.java
@@ -5,9 +5,80 @@
 package carrental;
 
 /**
- *
- * @author hypesystem
+ * Customer holds holds information on each customer
+ * @author CNN
+ * @version 25 Nov. 2011
  */
-public class Customer {
+public class Customer //impplements upgradeable
+{
+    private final int ID;
+    private int telephone;
+    private String name;
+    private String adress;
+    private String eMail;
     
+    /**
+     * Constructor for Customer
+     * @param ID
+     * @param telephone
+     * @param name
+     * @param adress
+     * @param eMail 
+     */
+    public Customer(int ID, int telephone, String name, String adress, String eMail){
+        this.ID = ID;
+        this.telephone = telephone;
+        this.name = name;
+        this.adress = adress;
+        this.eMail = eMail;
+    }
+    
+    /**
+     * Upgrade object information
+     * @param telephone
+     * @param name
+     * @param adress
+     * @param eMail 
+     */
+    public void upgradeObject(int telephone, String name, String adress, String eMail){
+        this.telephone = telephone;
+        this.name = name;
+        this.adress = adress;
+        this.eMail = eMail;
+    }
+    /**
+     * 
+     * @return Customer ID
+     */
+    public int getID(){
+        return ID;
+    }
+    /**
+     * 
+     * @return Telephone number
+     */
+    public int getTelephone(){
+        return telephone;
+    }
+    /**
+     * 
+     * @return Name
+     */
+    public String getName(){
+        return name;
+    }
+    /**
+     * 
+     * @return Adress
+     */
+    public String getAdress(){
+        return adress;
+    }
+    /**
+     * 
+     * @return E-Mail
+     */
+    public String getEMail(){
+        return eMail;
+    }
 }
