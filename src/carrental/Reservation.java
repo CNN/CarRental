6eb40b3515ed32f541e17988102ccfa55fc22769
Claus L. Henriksen <clih@itu.dk>
2011-11-25 20:58:48 +0100

    Reservation completed

--- src/carrental/Reservation.java
+++ src/carrental/Reservation.java
@@ -5,9 +5,79 @@
 package carrental;
 
 /**
- *
- * @author hypesystem
+ * Reservation holds holds information on a reservation
+ * @author CNN
+ * @version 25 Nov. 2011
  */
 public class Reservation implements Booking {
+    private final int ID;
+    private int vehicleID;
+    private long tStart;
+    private long tEnd;
+    private int customerID;
     
+    /**
+     * Constructor for reservation class
+     * @param ID
+     * @param vehicleID
+     * @param tStart
+     * @param tEnd
+     * @param customerID 
+     */
+    public Reservation(int ID, int vehicleID, long tStart, long tEnd, int customerID){
+        this.ID = ID;
+        this.vehicleID = vehicleID;
+        this.tStart = tStart;
+        this.tEnd = tEnd;
+        this.customerID = customerID;
+    }
+    
+    /**
+     * Update reservation object
+     * @param vehicleID
+     * @param tStart
+     * @param tEnd
+     * @param customerID 
+     */
+    public void updateObject(int vehicleID, long tStart, long tEnd, int customerID){
+        this.vehicleID = vehicleID;
+        this.tStart = tStart;
+        this.tEnd = tEnd;
+        this.customerID = customerID;
+    }
+    /**
+     * 
+     * @return Reservation ID
+     */
+    public int getID(){
+        return ID;
+    }
+    /**
+     * 
+     * @return Vehicle ID
+     */
+    public int getVehicleID(){
+        return vehicleID;
+    }
+    /**
+     * 
+     * @return Time start
+     */
+    public long getTStart(){
+        return tStart;
+    }
+    /**
+     * 
+     * @return Time end
+     */
+    public long getTEnd(){
+        return tEnd;
+    }
+    /**
+     * 
+     * @return Customer ID
+     */
+    public int getCustomerID(){
+        return customerID;
+    }
 }
