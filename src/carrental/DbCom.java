d6691b9ceb2ba8cfbdfd7a7ef0048e1a8efe9ee5
Claus L. Henriksen <clih@itu.dk>
2011-11-27 00:29:51 +0100

    Added SQL package to NetBeans projekt

--- src/carrental/DbCom.java
+++ src/carrental/DbCom.java
@@ -1,79 +1,69 @@
-///**
-// * @(#)DbCom.java
-// *              <---- needs project name
-// * @author SGN  <---- needs study group name
-// * @author CNN  <---- Claus Niels Niclas
-// * @version 1.00 2011/11/18
-// */
-//
-//
-//public class DbCom {
-//
-//    public DbCom() {
-//    	//initialise connection
-//    	//run
-//    	//close connection
-//    }
-//
-//    /**
-//     * @param Start date and end date
-//     * @return reservations within the given dates
-//     */
-//     public somehow getReservations(Date start, Date end){ //Would you prefer one get-method with table as param?
-//
-//     }
-//
-//    /**
-//     * Adds entry in table
-//     * @param String , String
-//     * @return unique carID or 0 if not succeeded
-//     */
-//    public int addCar(String tableName, fieldNames){ //methods needs try catch
-//    	int pk = createPK(Car); //remember to check !0
-//    	//more code
-//
-//    	INSERT INTO Car VALUES(PK, ) //more values
-//
-//    	return pk; //or zero
-//    }
-//
-//    /**
-//     * Adds reservation to reservation table
-//     * @param stuff
-//     * @return succeeded
-//     */
-//    public boolean addReservation(stuff){
-//    	int pk = createPK(Reservation); //remember to check !0
-//		INSERT INTO Reservation VALUES(PK, )
-//    }
-//
-//    /**
-//     * Edits reservation
-//     * @param reservation name or id?, new fields
-//     * @return succeeded
-//     */
-//     public boolean editReservation(stuff){
-//
-//     }
-//
-//     /**
-//      * Deletes reservation
-//      * @param reservation to delete
-//      * @return succeeded
-//      */
-//      public boolean deleteReservation(stuff){
-//
-//      }
-//
-//     /**
-//      * Creates primary key for next entry in a given table
-//      * @param table to check
-//      * @return int PK
-//      */
-//      private int createPK(table){
-//      	int pk = 0;
-//      	//code
-//
-//      	return pk;
-//      }
-//}
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package carrental;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Communicator object for controller and database
+ * @author CNN
+ * @version 25 Nov. 2011
+ */
+public class DbCom {
+    private Connection conn;
+    private Statement dbStatement;
+    
+    public DbCom(){
+    	openDb(); //create connection
+    	doStuff(); //run
+    	closeDb();
+    }
+    
+    public static void main(String[] args){
+        DbCom com = new DbCom();
+    }
+    
+    private void doStuff(){
+        try {
+            dbStatement = conn.createStatement();
+        } catch (SQLException ex) {
+            Logger.getLogger(DbCom.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        String query = "SELECT name FROM  vehicletype;";
+        try {
+            boolean ok = dbStatement.execute(query);
+            if (ok) {
+                ResultSet res = dbStatement.getResultSet();
+                while (res.next()) { 
+                    System.out.println(""+ res.getString("name"));
+                }
+            }
+        } catch (SQLException exn) {
+            System.out.println("Kan ikke lÊse fra database: " + exn);   
+        }
+    }
+    
+    private void openDb() {
+        try {
+            // Fra mysql-connector-java-5.1.5-bin.jar, lagt i /program files/java/jdk1.7.0_01/jre/lib/ext/
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/carrental", "root", "1991");
+        } catch (SQLException exn) {
+            System.out.println("Kan ikke Âbne databaseforbindelsen: " + exn);
+        } catch (ClassNotFoundException exn) {
+            System.out.println("Kan ikke finde databasedriver: " + exn);
+        }
+    }
+    
+    private void closeDb(){
+        try{
+            if(conn != null)
+                conn.close();   
+             }catch (SQLException exn){ 
+                 System.out.println("Kan ikke lukke databaseforbindelsen: " + exn);
+             }
+    }
+}
