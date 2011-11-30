f21816ac7eaad276ef221c4810e5d352ac309f5f
Niels Abildgaard <niels.abildgaard@gmail.com>
2011-11-30 13:23:46 +0100

    Maintenance written. Booking.java updated.

--- src/carrental/Maintenance.java
+++ src/carrental/Maintenance.java
@@ -1,14 +1,53 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package carrental;
+
+import java.sql.Timestamp;
 
 /**
  *
- * @author hypesystem
+ * @author CNN
  */
-public class Maintenance //implements Booking , Updateable 
-{
+public class Maintenance implements Booking {
+    private int id;
+    private int vehicle_id;
+    private Timestamp date_start;
+    private Timestamp date_end;
+    private int type_id;
+    
+    public Maintenance(int id, int vehicle_id, Timestamp date_start, Timestamp date_end, int type_id) {
+        if(date_end.after(date_start)) {
+            this.date_start = date_start;
+            this.date_end = date_end;
+            this.type_id = type_id;
+            this.id = id;
+            this.vehicle_id = vehicle_id;
+        }
+        else CarRental.getInstance().appendLog("Dates supplied for Maintenance #"+id+" in constuctor not valid. End date must be AFTER start date.");
+    }
+    
+    public void updateObject(int id, int vehicle_id, Timestamp date_start, Timestamp date_end, int type_id) {
+        if(date_end.after(date_start)) {
+            this.date_start = date_start;
+            this.date_end = date_end;
+            this.type_id = type_id;
+            this.vehicle_id = vehicle_id;
+        }
+        else CarRental.getInstance().appendLog("Dates supplied for Maintenance #"+id+" in updateObject not valid. End date must be AFTER start date.");
+    }
+    
+    public Timestamp getTEnd() {
+        return date_end;
+    }
+    
+    public Timestamp getTStart() {
+        return date_start;
+    }
+    
+    public int getID() {
+        return id;
+    }
+    
+    public int getVehicleID() {
+        return vehicle_id;
+    }
     
 }
