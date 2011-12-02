d5c09c412afc6ed2e4348c30841f779771a07328
Niels Abildgaard <niels.abildgaard@gmail.com>
2011-12-02 01:26:20 +0100

    Added versioning info and javadoc description of the current contents of Model.java

--- src/carrental/Model.java
+++ src/carrental/Model.java
@@ -3,8 +3,9 @@ package carrental;
 import java.util.ArrayList;
 
 /**
- *
- * @author hypesystem
+ * The model keeps track of the logic of CarRental
+ * @author CNN
+ * @version 2011-12-02
  */
 public class Model {
     private DbCom database;
@@ -14,14 +15,30 @@ public class Model {
         database = new DbCom();
     }
     
+    /**
+     * Get a vehicle matching an id
+     * @param id
+     * @return Vehicle
+     */
     public Vehicle getVehicle(int id) {
         ArrayList<String> v = database.getFirstMatch("SELECT * FROM vehicle WHERE id = '"+id+"'");
         return new Vehicle(Integer.parseInt(v.get(0)),Integer.parseInt(v.get(1)),v.get(2),v.get(3),Integer.parseInt(v.get(4)),v.get(5));
     }
+    
+    /**
+     * Get the first vehicle matching the name provided
+     * @param name
+     * @return Vehicle
+     */
     public Vehicle getVehicle(String name) {
         ArrayList<String> v = database.getFirstMatch("SELECT * FROM vehicle WHERE name = '"+name+"'");
         return new Vehicle(Integer.parseInt(v.get(0)),Integer.parseInt(v.get(1)),v.get(2),v.get(3),Integer.parseInt(v.get(4)),v.get(5));
     }
+    
+    /**
+     * Get an array of all vehicles in the database
+     * @return Array of vehicles
+     */
     public ArrayList<Vehicle> getVehicles() {
         ArrayList<ArrayList<String>> vs = database.getMatches("SELECT * FROM vehicle ORDER BY type DESC");
         ArrayList<Vehicle> results = new ArrayList<>();
