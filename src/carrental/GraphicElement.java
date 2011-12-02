62a5d5003be14c2c40b209e559915aa9ab3b1fe7
Claus L. Henriksen <clih@itu.dk>
2011-12-02 12:57:44 +0100

    Added empty GraphicElemtn.java

---
+++ src/carrental/GraphicElement.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package carrental;
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ * Graphic element for GUI
+ * @author CNN
+ * @version 2. Dec. 2011
+ */
+public class GraphicElement extends JComponent{
+    
+}
