fb11bee9cd5bbed39629d503a47f223b089f3f04
Niels Abildgaard <niels.abildgaard@gmail.com>
2011-12-17 17:03:45 +0100

    ControllerTest

--- tests/carrental/ControllerTest.java
+++ tests/carrental/ControllerTest.java
@@ -25,4 +25,76 @@ public class ControllerTest {
         String[] splitLog = controller.getLog().split("\n");
         assertEquals(splitLog[splitLog.length - 1].trim(),testStr);
     }
+    
+    /**
+     * Tests whether the requested object exists, and is infact a vehicle
+     */
+    @Test
+    public void testRequestVehicle() {
+        boolean success = false;
+        if(controller.requestVehicle(controller.requestNewVehicleId() - 1) != null &&
+                controller.requestVehicle(controller.requestNewVehicleId() - 1) instanceof Vehicle)
+            success = true;
+        assertEquals(success, true);
+    }
+    
+    /**
+     * Tests whether the requested object exists, and is infact a vehicle type
+     */
+    @Test
+    public void testRequestVehicleType() {
+        boolean success = false;
+        if(controller.requestVehicleType(controller.requestNewVehicleTypeId() - 1) != null &&
+                controller.requestVehicleType(controller.requestNewVehicleTypeId() - 1) instanceof VehicleType)
+            success = true;
+        assertEquals(success, true);
+    }
+    
+    /**
+     * Tests whether the requested object exists, and is infact a maintenance
+     */
+    @Test
+    public void testRequestMaintenance() {
+        boolean success = false;
+        if(controller.requestMaintenance(controller.requestNewMaintenanceId() - 1) != null &&
+                controller.requestMaintenance(controller.requestNewMaintenanceId() - 1) instanceof Maintenance)
+            success = true;
+        assertEquals(success, true);
+    }
+    
+    /**
+     * Tests whether the requested object exists, and is infact a maintenance type
+     */
+    @Test
+    public void testRequestMaintenanceType() {
+        boolean success = false;
+        if(controller.requestMaintenanceType(controller.requestNewMaintenanceTypeId() - 1) != null &&
+                controller.requestMaintenanceType(controller.requestNewMaintenanceTypeId() - 1) instanceof MaintenanceType)
+            success = true;
+        assertEquals(success, true);
+    }
+    
+    /**
+     * Tests whether the requested object exists, and is infact a reservation
+     */
+    @Test
+    public void testRequestReservation() {
+        boolean success = false;
+        if(controller.requestReservation(controller.requestNewReservationId() - 1) != null &&
+                controller.requestReservation(controller.requestNewReservationId() - 1) instanceof Reservation)
+            success = true;
+        assertEquals(success, true);
+    }
+    
+    /**
+     * Tests whether the requested object exists, and is infact a customer
+     */
+    @Test
+    public void testRequestCustomer() {
+        boolean success = false;
+        if(controller.requestCustomer(controller.requestNewCustomerId() - 1) != null &&
+                controller.requestCustomer(controller.requestNewCustomerId() - 1) instanceof Customer)
+            success = true;
+        assertEquals(success, true);
+    }
 }
